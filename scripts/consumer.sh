508661321b2d5aa5b2ab94128c17a8780b21cf10
OldRabbit736 <sylvan0212@gmail.com>
2022-10-02 17:34:35 +0900

    completed: #8 - Create REST API to Send Message

--- scripts/consumer.sh
+++ scripts/consumer.sh
@@ -1,2 +1,8 @@
-cd ~/apps/kafka
-bin/kafka-console-consumer.sh --topic quickstart-events --from-beginning --bootstrap-server localhost:9092
+# https://unix.stackexchange.com/questions/25945/how-to-check-if-there-are-no-parameters-provided-to-a-command
+if [ $# -eq 0 ]; then
+  echo "No arguments provided"
+  exit 1
+fi
+
+kafka_topic=$1
+~/apps/kafka/bin/kafka-console-consumer.sh --topic "${kafka_topic}" --from-beginning --bootstrap-server localhost:9092
